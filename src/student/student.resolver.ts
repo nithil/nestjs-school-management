0d5cd5fd48ab13a59945afe802e2ec0f81da5e7f
Nithil N <nithiln96@gmail.com>
2020-05-26 20:36:04 +0530

    Student creation mutation

---
+++ src/student/student.resolver.ts
@@ -0,0 +1,15 @@
+import { Resolver, Mutation, Args } from '@nestjs/graphql';
+
+import { StudentType } from './student.type';
+import { CreateStudentInput } from './create-student.input';
+import { StudentService } from './student.service';
+
+@Resolver(_of => StudentType)
+export class StudentResolver {
+  constructor(private studentService: StudentService) {}
+
+  @Mutation(_returns => StudentType)
+  async createStudent(@Args('createStudentInput') createStudentInput: CreateStudentInput) {
+    return this.studentService.create(createStudentInput);
+  }
+}
--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,24 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { v4 as uuid } from 'uuid';
+import { Repository } from 'typeorm';
+
+import { CreateStudentInput } from './create-student.input';
+import { Student } from './student.entity';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+  constructor(@InjectRepository(Student) private studentRepository: Repository<Student>) {}
+
+  async create(createStudentInput: CreateStudentInput): Promise<Student> {
+    const { firstName, lastName } = createStudentInput;
+
+    const student = this.studentRepository.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+
+    return await this.studentRepository.save(student);
+  }
+}
